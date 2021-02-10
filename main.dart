67d141deac039b339c9338a8abc5fa8568350276
Sooyoungro Church <syrch@Sooyoungroui-MacBook-Air.local>
2021-02-10 14:26:46 +0900

    ios

--- main.dart
+++ main.dart
@@ -10,8 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BBANTO',
-      theme: ThemeData(
-      ),
+      theme: ThemeData(),
       home: Grade(),
     );
   }
@@ -26,28 +25,26 @@ class Grade extends StatelessWidget {
         title: Text('BBANTO'),
         backgroundColor: Colors.amber[700],
         centerTitle: true,
-        elevation:0.0,
+        elevation: 0.0,
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
           children: [
-            Text('NAME',
-            style: TextStyle(
-              color:Colors.white,
-              letterSpacing: 2.0
-            ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
             ),
             SizedBox(
-              height:10.0,
+              height: 10.0,
             ),
-            Text('LEECHANGIL',
-            style: TextStyle(
-              color:Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
+            Text(
+              'LEECHANGILIOS',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -55,4 +52,3 @@ class Grade extends StatelessWidget {
     );
   }
 }
-
